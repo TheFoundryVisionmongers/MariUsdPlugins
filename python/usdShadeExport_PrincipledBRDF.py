189ece8fcc6215fa3d708846bcd7563282de185d
Kiyoyuki Nakagaki <kiyo.nakagaki@foundry.com>
2022-11-01 12:51:23 +0900

    TP-518191 Show Progress Dialog while exporting textures for Principled BRDF USD Look file export

--- python/usdShadeExport_PrincipledBRDF.py
+++ python/usdShadeExport_PrincipledBRDF.py
@@ -127,7 +127,7 @@ def writePrincipledBRDFSurface(looks_stage, usd_shader, usd_export_parameters, u
                 usd_export_parameters.exportRootPath()
             )
         )
-        mari.exports.exportTextures(export_items, usd_export_parameters.exportRootPath())
+        mari.exports.exportTextures(export_items, usd_export_parameters.exportRootPath(), ShowProgressDialog = True)
 
 class Principled_BRDF_SettingsWidget(widgets.QWidget):
     def __init__(self, parent = None):
